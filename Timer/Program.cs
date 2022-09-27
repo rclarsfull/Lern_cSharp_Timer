a412674e124d7f262b2943af6fcf379e19bd54c1
Lars Przybylek <l.p.1999@live.de>
2022-09-27 22:03:30 +0200

    Initial commit

---
+++ Timer/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+namespace Timer;
+class Timer
+{
+    public static void Main()
+    {
+        string input = "";
+        string command = "";
+        string commandName = "";
+        int commandTime = 0;
+        string[] commands = input.Split(" ");
+        ArrayList threads = new ArrayList();
+        bool valid = true;
+        
+        
+        while (true)
+        {
+            input = Console.ReadLine();
+            if (commands.Length!=3)
+            {
+                Console.WriteLine("Falsche Eingabe!!\n Eingabe wird ignoriert.");
+                valid = false;
+            }
+
+            command = commands[0];
+            commandName = commands[1];
+            valid=Int32.TryParse(commands[2], out commandTime);
+
+            if (valid &&command.Equals("startTimer"))
+            {
+                TimerThread thread = new TimerThread(commandName, commandTime);
+                Thread temp = new Thread(thread.runTimer());
+                temp.Start();
+                threads.Add(temp);
+            }
+
+            valid = true;
+        }
+    }
+}
---
+++ Timer/TimerThread.cs
@@ -0,0 +1,36 @@
+﻿namespace Timer;
+using System.Threading;
+
+public class TimerThread
+{
+    private string name;
+    private int seconds;
+
+    public TimerThread(string name, int seconds)
+    {
+        this.name = name;
+        this.seconds = seconds;
+    }
+    public void runTimer()
+    {
+        try
+        {
+            DateTime start=DateTime.Now;
+            DateTime end = start.Add(TimeSpan.FromSeconds(seconds));
+            Console.WriteLine("The Timer "+name+" started at: "+start.Hour+":"+start.Second+"and will end at"+end.Hour +":"+ end.Second);
+            Console.WriteLine(start.CompareTo(end));
+            while (start.CompareTo(end)>1)
+            {
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("The Time of Timer "+name+ "is over!! ("+end.Subtract(start)+")");
+
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Time Thread Exeption:\n"+e);
+            throw;
+        }
+    }
+}
